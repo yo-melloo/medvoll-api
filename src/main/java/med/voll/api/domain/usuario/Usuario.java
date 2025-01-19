6b2686bef906644a982b42eebed658375cb3fd73
Gustavo Mello <oliver7mello@gmail.com>
2025-01-19 20:52:39 -0300

    Implementando configurações de segurança

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "usuario")
+@Getter //--> não funcionou na minha máquina
+@NoArgsConstructor //--> não funcionou
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+}
