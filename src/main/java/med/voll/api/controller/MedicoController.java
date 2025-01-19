45d9889253e12da8f75fc969eee02166a66ca7f4
Gustavo Mello <oliver7mello@gmail.com>
2025-01-19 13:32:34 -0300

    Implemetando validação de dados no código

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,9 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,8 @@ public class MedicoController {
     MedicoRepository repositorio;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repositorio.save(new Medico(dados));
     }
 
