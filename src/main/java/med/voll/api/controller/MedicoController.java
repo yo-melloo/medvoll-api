96365bbe1ff5eff51d6f27e48e7945705ce375a0
Gustavo Mello <oliver7mello@gmail.com>
2025-01-19 12:54:04 -0300

    Erro na minha maquina - Lombok não gera construtor vazio

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    MedicoRepository repositorio;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico json){
-        System.out.println("Dados recebidos: " + json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        repositorio.save(new Medico(dados));
     }
 
 }
--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+//@NoArgsConstructor -> Não funcionou
 @AllArgsConstructor
-@NoArgsConstructor
 public class Endereco {
 
     private String logradouro;
@@ -19,4 +19,15 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+
+    public Endereco() {}
 }
